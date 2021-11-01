eaaf9f673d116f1bacc3366b9111919b645c2dec
Naris <naris2172@gmail.com>
2021-11-01 20:26:49 +0700

    admin page UI

--- build/public/admin.js
+++ build/public/admin.js
@@ -1,9 +1,5 @@
 const socket = io();
 
-
-
-
-
 socket.emit('admin')
 
 socket.on('population', (users)=>{
@@ -12,24 +8,26 @@ socket.on('population', (users)=>{
     const div = document.createElement('div');
     const pop = document.querySelector('.population')
     pop.innerHTML = 
-    `<h3>Online Users: ${users.length}</h3>
-    <h3>Online Players: ${players.length}</h3>
-    <h3>Spectators: ${users.length - players.length}</h3>`
+    `<h3 style="font-size:25px;">Online Users: ${users.length}</h3>
+    <h3 style="font-size:25px;">Online Players: ${players.length}</h3>
+    <h3 style="font-size:25px;">Spectators: ${users.length - players.length}</h3><h3 id='rooms' style="font-size:40px;">Current Rooms</h3>`
 })
-
 socket.on('adminRoom', rooms =>{
+    
     const roomsDiv = document.querySelector('.rooms')
     roomsDiv.innerHTML = ''
+   
     for (const roomName in rooms){
         console.log(roomName)
         const div = document.createElement('div')
-        div.className = roomName
+        div.className = "allRooms"
         div.innerHTML = `
-        <h2>Room Name: ${roomName}</h2>
-        <button id="${"rs" + roomName}" class="rs">Reset Game</button>
+        <h2 style="font-size:25px;">Room Number: ${roomName}</h2>
+        <button id="${"rs" + roomName}" class="rs" style="font-family: 'Games', sans-serif;">Reset Game</button>
         `
         roomsDiv.appendChild(div)
-    }    
+    }
+    
     for(const control of document.querySelectorAll(".rs")){
         control.addEventListener("click", function(){
             handleReset(this.getAttribute('id'))
